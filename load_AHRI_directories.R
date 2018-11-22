54fc79045751e23fe678873fd8976aa2044b693b
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-11-22 15:17:17 -0800

    500 AHRI records not the expected 354

--- load_AHRI_directories.R
+++ load_AHRI_directories.R
@@ -9,4 +9,22 @@ source("setup.R")
 # set up paths to working directories
 wd_AHRI_dirs <- "/home/jiml/HotWaterResearch/projects/CECHWT24/2019 ACM tankless/AHRI/2018-11-22"
 
+# find the xlsx files there
+l_fn <-
+  list.files(path = wd_AHRI_dirs, pattern = ".xlsx", full.names = TRUE)
+
+# read the files
+DT_AHRI_dir <-
+  data.table(ldply(l_fn, read_excel, .progress = "text"))
+
+# see what's there
+names(DT_AHRI_dir)
+nrow(DT_AHRI_dir)
+# [1] 500
+# why 500?
+
+# number of AHRI Certified Reference Number
+DT_AHRI_dir[ , list(nrefnum = length(unique(`AHRI Certified Reference Number`)))]
+# 1:     250
+# accidentally downloaded same thing twice?
 
