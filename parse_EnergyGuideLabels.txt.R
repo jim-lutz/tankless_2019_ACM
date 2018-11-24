3a6f3b9b6f11c4aa59fd5900cf5b2fedd135333c
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-11-24 12:10:42 -0800

    got MaxGPM

--- parse_EnergyGuideLabels.txt.R
+++ parse_EnergyGuideLabels.txt.R
@@ -20,7 +20,7 @@ for( fn in 1:length(l_fn) ) {
   # for production use 1:length(l_fn)
   
   # show the filename
-  cat(l_fn[fn],"\n")
+  # cat(l_fn[fn],"\n")
   
   # get the AHRIrefnum from the filename
   DT_EGL[fn, AHRIrefnum := str_extract(l_fn[fn], "[0-9]{6,}" )]
@@ -33,9 +33,31 @@ for( fn in 1:length(l_fn) ) {
   
   # put into the data.table for debugging later
   DT_EGL[fn, EGL := EGLtxt]
+
 }
 
 DT_EGL 
-DT_EGL[c(1,5,22,150,336,340), list(AHRIrefnum, 
-                                   EGLtxt = str_sub(EGLtxt, 1, 20)
-                                   )] 
+
+# here's a sample of files to examine while developing parsing commands
+# mousepad data/tiff/10014414-001.txt &
+# mousepad data/tiff/4397486-001.txt &
+# mousepad data/tiff/9970164-001.txt &
+# mousepad data/tiff/10014415-001.txt &
+
+# now build items of interest from the data.table
+
+# get the MaxGPM
+DT_EGL[, MaxGPM := str_extract(EGL, "\\:.+ g[op]m Model")]
+DT_EGL[, MaxGPM := as.numeric(str_sub(MaxGPM, 2, 5))]
+
+# look at some entries in the data.table
+DT_EGL[AHRIrefnum %in% c('10014414', '4397486', '9970164', '10014415'),
+       list(AHRIrefnum, 
+            EGLtxt = str_sub(EGLtxt, 1, 20),
+            MaxGPM
+            )
+       ] 
+
+# look at all the MaxGPMs
+DT_EGL[, list(n = length(AHRIrefnum)),
+       by=MaxGPM][order(MaxGPM)]
