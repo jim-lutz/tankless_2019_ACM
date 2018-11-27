7c814a57dd4e5a90fa5663a1028614b0b528a82d
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-11-27 08:16:14 -0800

    was saving wrong data.table names

--- combine_directories.R
+++ combine_directories.R
@@ -14,19 +14,19 @@ write_csv(data.table(AHRInames = names(DT_AHRI)),
 # Energy Guide labels
 load("data/DT_EGL.Rdata")
 names(DT_EGL)
-write_csv(data.table(EGLnames = names(DT_AHRI)), 
+write_csv(data.table(EGLnames = names(DT_EGL)), 
           path = "data/EGLnames.csv")
 
 # DOE directory
 load("data/DT_DOE_dir.Rdata")
 names(DT_DOE_dir)
-write_csv(data.table(DOEnames = names(DT_AHRI)), 
+write_csv(data.table(DOEnames = names(DT_DOE_dir)), 
           path = "data/DOEnames.csv")
 
 # CEC directory
 load("data/DT_CEC_dir.Rdata")
 names(DT_CEC_dir)
-write_csv(data.table(CECnames = names(DT_AHRI)), 
+write_csv(data.table(CECnames = names(DT_CEC_dir)), 
           path = "data/CECnames.csv")
 
 
